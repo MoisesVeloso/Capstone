39dee123a3a4f21dc59d05fdb5f820b11c8c0f9e
Moises Veloso <dev.moises09@gmail.com>
2023-12-04 14:08:27 +0800

    Update

--- qrcode.php
+++ qrcode.php
@@ -4,7 +4,7 @@ include('phpqrcode/qrlib.php');
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['brand'])) {
     $equipment = $_POST['brand'];
 
-    $qrCodeData = "Equipment: $equipment, Link: 192.168.1.8/Capstone/form.php?brand=$equipment";
+    $qrCodeData = "Equipment: $equipment, Link: 192.168.89.100/Capstone/form.php?brand=$equipment";
 
     $filename = 'qrcodes/' . uniqid('qr_') . '.png';
 
